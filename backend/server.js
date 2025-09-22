ac8792319208feba6a1b177593f4b3b88ebf284a
Editorhacker <94226999+Editorhacker@users.noreply.github.com>
2025-09-22 23:35:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require("./src/app");
 
 
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000; // Use Render's PORT if available
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
