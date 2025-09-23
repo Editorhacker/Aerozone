22ec926d7ff81ff71bfc5f9516d7a2561a0cf588
Editorhacker <94226999+Editorhacker@users.noreply.github.com>
2025-09-23 21:33:00 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,14 @@ const cors = require("cors");
 const dataRoute = require("../routes/dataRoute");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://aerozone.vercel.app", // allow only your Vercel domain
+  credentials: true,
+}));
 app.use(express.json());
 
 
 app.use("/api/data", dataRoute);
+
 
 module.exports = app;
